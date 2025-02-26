3b083ac789986ce00f14c8365585424f05ef20ac
Aayush08102013 <aayush.ranjan0815@gmail.com>
2025-02-26 18:44:22 +1300

    Add files via upload

---
+++ first.py
@@ -0,0 +1,10 @@
+import keyword
+print("Ranjan", "Aayush")
+print(443,"marks out of 445 \n", end="*")
+#Data types, variables, print statment, formatted string, \n.
+name = input("enter your name please")
+age = input("enter your age please")
+print(f"\n The name of is {name}, who is {age} years old")
+
+print("the keywords in python are: \n")
+print(keyword.kwlist)
